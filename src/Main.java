a8fcceba82d9886076358b9748a4055672a59dcd
yarik <yarikshtoda1988@gmail.com>
2022-12-28 01:10:01 +0300

    Коддинг курсовая 2

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            int data = 50 / 0;
+        } catch (Exception e) {
+            System.out.println("Нельзя делить на ноль");
+        }
+
+
+    }
+
+}
